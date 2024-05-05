71475077cdcc425843ac99bfa9f865b7be426b99
chacha86 <taejincha0619@gmail.com>
2024-05-05 15:27:43 +0900

    recursive test

--- src/test/java/com/example/ms1/Ms1ApplicationTests.java
+++ src/test/java/com/example/ms1/Ms1ApplicationTests.java
@@ -8,6 +8,24 @@ class Ms1ApplicationTests {
 
     @Test
     void contextLoads() {
+//        hi();
+        hiReccursive(5);
+    }
+
+    void hiReccursive(int n) {
+        // 재귀 종료 조건
+        if(n == 0) {
+            return;
+        }
+
+        System.out.println("hi");
+        hiReccursive(n - 1);
+    }
+
+    void hi() {
+        for(int i = 0; i < 5; i++) {
+            System.out.println("hi");
+        }
     }
 
 }
